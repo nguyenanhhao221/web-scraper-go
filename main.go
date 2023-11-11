c5b7ac700a044b58828b10f9cfdd3fa59befaf23
Hao Nguyen <hao@haonguyen.tech>
2023-11-11 15:49:36 +0700

    feat: add demo scraper function

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
+// Package main
+// https://www.zenrows.com/blog/web-scraping-golang#visit-target-html-page
 package main
 
-import "log"
+import (
+	"log"
+	"sync"
+	"time"
+)
 
 func main() {
-	log.Println("Hello world")
+	log.Println("Welcome to Hao Go Lang Web Scraper")
+
+	urlToVisit := "https://scrapeme.live/shop/"
+	timeBetweenRequest := time.Second * 5
+
+	// Implement a wait group
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+
+	go startScraping(urlToVisit, timeBetweenRequest, wg)
+
+	wg.Wait()
 }
